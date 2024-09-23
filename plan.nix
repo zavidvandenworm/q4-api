5591f74e7fc44e20d54a521df0602f66b7a061a4
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2024-09-23 11:53:38 +0200

    dockerfile

--- plan.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.openssl
-    pkgs.openssl_1_1   # For backward compatibility with older libraries
-    pkgs.dotnet-sdk    # .NET SDK
-  ];
-}
