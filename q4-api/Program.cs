3ebef43cee1026ea5967ca81a8ab988ce544c381
Dev Dev <suggestied@gmail.com>
2024-09-24 09:23:26 +0200

    always swager

--- q4-api/Program.cs
+++ q4-api/Program.cs
@@ -16,13 +16,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
-}
+
 
 // no cors
 app.UseCors(
